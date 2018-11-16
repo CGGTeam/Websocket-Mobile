a64fec0538366e7b3adff040ce74694c5c9daff5
von3007 <von3007@live.ca>
2018-11-16 09:04:02 -0500

    Initial commit

---
+++ Project/app/src/main/java/cgodin/qc/ca/projet/ExamenFragment.java
@@ -0,0 +1,45 @@
+package cgodin.qc.ca.projet;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class ExamenFragment extends Fragment {
+    public ExamenFragment() {
+        // Required empty public constructor
+    }
+    public static CombatFragment newInstance(String param1, String param2) {
+        CombatFragment fragment = new CombatFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_examen, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+}
